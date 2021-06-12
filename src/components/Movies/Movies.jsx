55e06196c192d1ad860e8cd0086a75e2c9bde9c2
Ник Николос <nic23173@gmail.com>
2021-06-12 15:19:56 +0300

    feat: create SearchForm

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -2,12 +2,15 @@ import './Movies.css';
 import Header from '../Header/Header';
 import Content from '../Content/Content';
 import Footer from '../Footer/Footer';
+import SearchForm from '../SearchForm/SearchForm';
 
 export default function Movies() {
   return (
     <div className='movies'>
       <Header />
-      <Content></Content>
+      <Content>
+        <SearchForm />
+      </Content>
       <Footer />
     </div>
   );
