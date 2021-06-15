c0f05dc625f3a074af8c1dfc236b7a4f52c13eea
Ник Николос <nic23173@gmail.com>
2021-06-15 22:39:34 +0300

    fix: change Navigation

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,34 @@
 import './Header.css';
 import Navigation from '../Navigation/Navigation';
 import Logo from '../Logo/Logo';
+import { Link } from 'react-router-dom';
 
-export default function Header() {
+export default function Header({ isLogged = true }) {
   return (
     <header className='header'>
       <Logo />
-      <Navigation isLogged={true} />
+      {isLogged ? (<Navigation />) : (
+        <nav className='header__nav'>
+          <ul className='header__ul'>
+            <li className='header__line-logout'>
+              <Link
+                className='header__register-link'
+                to='/signup'
+              >
+                Регистрация
+              </Link>
+            </li>
+            <li className='header__line-logout'>
+              <Link
+                className='header__login-link'
+                to='/signin'
+              >
+                Войти
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      )}
     </header>
   );
 }
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,60 +1,39 @@
 import './Navigation.css';
-import { Link, NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
-export default function Navigation({ isLogged }) {
+export default function Navigation() {
   return (
-    <nav>
-      {isLogged ? (
-        <ul className='navigation'>
-          <li className='navigation__line'>
-            <NavLink
-              className='navigation__link'
-              activeClassName="navigation__link_active"
-              to='/movies'
-            >
-              Фильмы
-            </NavLink>
-          </li>
-          <li className='navigation__line'>
-            <NavLink
-              className='navigation__link'
-              activeClassName="navigation__link_active"
-              to='/saved-movies'
-            >
-              Сохраненные фильмы
-            </NavLink>
-          </li>
-          <li className='navigation__line'>
-            <NavLink
-              className='navigation__link'
-              activeClassName="navigation__link_active"
-              to='/profile'
-            >
-              Аккаунт
-              <div className="navigation__icon"/>
-            </NavLink>
-          </li>
-        </ul>
-      ) : (
-        <ul className='navigation'>
-          <li className='navigation__line-logout'>
-            <Link
-              className='navigation__register-link'
-              to='/signup'
-            >
-              Регистрация
-            </Link>
-          </li>
-          <li className='navigation__line-logout'>
-            <Link
-              className='navigation__login-link'
-              to='/signup'
-            >
-              Войти
-            </Link>
-          </li>
-        </ul>
-      )}
+    <nav className='navigation'>
+      <ul className='navigation__ul'>
+        <li className='navigation__line'>
+          <NavLink
+            className='navigation__link'
+            activeClassName="navigation__link_active"
+            to='/movies'
+          >
+            Фильмы
+          </NavLink>
+        </li>
+        <li className='navigation__line'>
+          <NavLink
+            className='navigation__link'
+            activeClassName="navigation__link_active"
+            to='/saved-movies'
+          >
+            Сохраненные фильмы
+          </NavLink>
+        </li>
+        <li className='navigation__line'>
+          <NavLink
+            className='navigation__link'
+            activeClassName="navigation__link_active"
+            to='/profile'
+          >
+            Аккаунт
+            <div className="navigation__icon"/>
+          </NavLink>
+        </li>
+      </ul>
     </nav>
   );
 }
